373b545b0cec07165433ee08659c47dbee30c921
“Kyle <kylemboomer@gmail.com>
2024-02-12 14:45:38 -0600

    Pass First Hobbit Test

--- mythical-creatures/test/hobbit-test.js
+++ mythical-creatures/test/hobbit-test.js
@@ -3,7 +3,7 @@ var {createHobbit, celebrateBirthday, getRing, meetPeople, findFriends} = requir
 
 describe('Hobbit', function() {
 
-  it.skip('should make a hobbit with a name and age', function() {
+  it('should make a hobbit with a name and age', function() {
     var bilbo = createHobbit('Bilbo', 0);
     var mark = createHobbit('Mark', 5);
 
@@ -14,7 +14,7 @@ describe('Hobbit', function() {
     assert.equal(mark.age, 5);
   });
 
-  it.skip('should start out 0 years old if not specified', function() {
+  it('should start out 0 years old if not specified', function() {
     var bilbo = createHobbit('Bilbo');
 
     assert.equal(bilbo.age, 0);
