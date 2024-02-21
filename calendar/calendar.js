65ffb3e25f48c96fffa1ce5b16ef011ddb96387a
“Kyle <kylemboomer@gmail.com>
2024-02-21 10:02:42 -0600

    Pass 3 Calendar Tests

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,24 @@
+function createEvent(title, month, day) {
+    var event = {
+        title: title,
+        month: month,
+        day : day
+    }
+    if (event.day >= 1 && event.day < 31) {
+        return event
+    } else {
+        return `Error: ${event.day} is not a valid day`
+    }
+}
 
+function createCalendar(owner, events) {
+    console.log('owner', owner)
+    console.log('events', events)
+    var calendar = {
+        owner: owner,
+        events: events
+    }
+ return calendar 
+}
 
-module.exports = {  };
+module.exports = {createEvent, createCalendar};
