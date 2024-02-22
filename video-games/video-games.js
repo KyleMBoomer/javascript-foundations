29009fcc9de3912b0d5b33305bbf37d68f825f8a
“Kyle <kylemboomer@gmail.com>
2024-02-22 11:41:04 -0600

    Passes 7 Tests

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,30 @@
+function createPlayer(name, age, moveset) {
+  var player = {
+    name: name,
+    age: age,
+    moveset: moveset
+  }
+  return player
+}
+
+function createLevel(name, players, coins, lives) {
+    var level = {
+        name: name,
+        players: players,
+        coins: 0 || 5,
+        lives: 3
+    }
+    return level
+}
+
+function findCoins(level) {
+var updatedLevel = level.coins += 5
+console.log('updatedlevel', updatedLevel)
+return updatedLevel
+}
 
 
 
-module.exports = { };
+
+
+module.exports = {createPlayer, createLevel, findCoins };
