68fa57ad9166383ce6ef9d17af6a150a4475c1a9
“Kyle <kylemboomer@gmail.com>
2024-02-27 17:59:54 -0600

    Pass All Tests

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,32 @@
+function createBarber(name, earnings, haircuts) {
+    var barber = {
+        name: name,
+        earnings: earnings || 0,
+        haircuts: haircuts || []
+    }
+    return barber
+}
+
+function giveCompliment(barber) {
+return `This ${barber.style} looks great!`
+}
+
+function cutHair(barber, haircut) {
+barber.haircuts.push(haircut)
+barber.earnings += haircut.price 
+return barber
+}
+
+function listStyles(barber) {
+var shortStyles = []
+for (var i = 0; i < barber.haircuts.length; i++) {
+    if (barber.haircuts[i].hairLength === 'short') {
+        shortStyles.push(barber.haircuts[i].style)
+    }
+console.log('array', shortStyles)
+  }   
+    return shortStyles
+}
 
 
-module.exports = {  };
+module.exports = { createBarber, giveCompliment, cutHair, listStyles };
