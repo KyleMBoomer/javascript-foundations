c36702eca6b45eb74dcecde256655fb6f763b15e
“Kyle <kylemboomer@gmail.com>
2024-02-29 10:41:19 -0600

    Pass All But One DJ Test

--- dj/dj.js
+++ dj/dj.js
@@ -1,8 +1,50 @@
+function createSong(name, artist, favorite, hasBeenPlayed) {
+  var song = {
+    name: name, 
+    artist: artist, 
+    favorite: favorite || false,
+    hasBeenPlayed: hasBeenPlayed || false 
+  }
+  return song
+}
+
+function playSong(song) {
+song.hasBeenPlayed = true
+return song
+}
+
+function makePlaylist(string, array) {
+var playlist = {
+  name: string,
+  songs: array
+}
+return playlist
+}
+
+function addSongToPlaylist(playlist, song) {
+playlist.songs.push(song)
+return playlist
+}
+
+function playSongs(playlist){
+  for (var i = 0; i < playlist.songs.length; i++) {
+playlist.songs[i].hasBeenPlayed = true;
+for (var j = 0; j < playlist.songs.length; j++) {
+  if (playlist.songs[i].favorite === false) {
+    playlist.songs[i].hasBeenPlayed = false
+      }
+      console.log('playlist loop2', playlist)
+   }
+   console.log('playlist loop1', playlist)
+}
+console.log('playlist return', playlist)
+return playlist
+}
 
 module.exports = { 
-  // createSong, 
-  // playSong, 
-  // makePlaylist, 
-  // addSongToPlaylist, 
-  // playSongs
+  createSong, 
+  playSong, 
+  makePlaylist, 
+  addSongToPlaylist, 
+  playSongs
 };
