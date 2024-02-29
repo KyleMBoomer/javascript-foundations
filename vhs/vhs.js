bcc29e4770ee5f4aa547fd12df147b34f3ecb342
“Kyle <kylemboomer@gmail.com>
2024-02-29 12:15:37 -0600

    Pass All But Two Tests

--- vhs/vhs.js
+++ vhs/vhs.js
@@ -1,3 +1,31 @@
+function createTape(title, readyToPlay) {
+    var tape = {
+        title : title,
+        readyToPlay: readyToPlay || false
+    }
+    return tape
+}
+
+function reset(tape) {
+    tape.readyToPlay = true
+    return tape
+}
+
+function createCollection(tape1, tape2, tape3) {
+var collection = [] || '' 
+if (collection === undefined) {
+    return 'Your collection is empty.'
+} else {
+    collection.push(tape1, tape2, tape3)
+}
+console.log(collection)
+return collection
+
+}
 
 
-module.exports = {  }
+module.exports = { createTape, 
+    reset, 
+    createCollection, 
+    // previewCollection
+ }
