75fea9350fd66284b12c8fd9e1e36720114f2b56
“Kyle <kylemboomer@gmail.com>
2024-03-21 17:30:07 -0500

    Pass All Elevator Tests

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,34 @@
+function createElevator(building, floors, currentFloor, passengers) {
+    return {
+        building: building,
+        floors: floors,
+        currentFloor: currentFloor,
+        passengers: passengers
+    }
+}
 
+function changeFloors(elevator, number) {
 
-module.exports = {  };
+    if(number === elevator.currentFloor) {
+        return `You\'re already on floor ${number}!`
+    }
+    if (elevator.floors < number) {
+       return `Floor ${number} does not exist!`
+    }
+return `Taking you to floor ${number}!`
+}
+
+function dropOffPassenger(elevator, passenger) {
+    for (var i = 0; i < elevator.passengers.length; i++) {
+        if(elevator.passengers[i] === passenger) {
+            elevator.passengers.splice(i, 1)
+        }
+    }
+    return elevator.passengers
+}
+module.exports = { 
+    createElevator,
+    changeFloors,
+    dropOffPassenger
+
+ };
