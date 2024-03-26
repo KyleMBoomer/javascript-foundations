5881816d37349a100f5a1c29216c06545ec38e43
“Kyle <kylemboomer@gmail.com>
2024-03-26 15:29:06 -0500

    Pass All Library Tests

--- library/library.js
+++ library/library.js
@@ -1,3 +1,36 @@
+function issueCard(name, age) {
+    if (age < 12) {
+return {
+    name: name,
+    age: age,
+    numBooksCheckedOut: 0,
+    isChild: true
+    }
+  } else {
+    return {
+    name: name,
+    age: age,
+    numBooksCheckedOut: 0,
+    isChild: false
+    }
+  }
+}
 
+function searchByAuthor(array, string) {  
+    console.log('collection', array)
+    var book = [] 
+    for (var i = 0; i < array.length; i++ ) {
+        if (array[i].author === string) {
+            book.push(array[i])
+        } 
+    } 
+    if (book.length) {
+    return book 
+    }
+    return 'No book found for search criteria'
+}
 
-module.exports = { };
+module.exports = {  
+    issueCard, 
+    searchByAuthor,
+}
