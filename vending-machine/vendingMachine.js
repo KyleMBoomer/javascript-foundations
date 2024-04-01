023cc6114c3d7e15afbabe64d5948d87fb7f1d5f
“Kyle <kylemboomer@gmail.com>
2024-04-01 10:54:07 -0500

    Pass All Tests

--- vending-machine/vendingMachine.js
+++ vending-machine/vendingMachine.js
@@ -1,2 +1,32 @@
+function createItemStock(name = 'unknown', stock = 0, price = 1.00) {
+    return {
+        name: name,
+        quantity: stock,
+        price: price
+    }
+}
 
-module.exports = {}
+function makePurchase(item, money) {
+    if (money < item.price) {
+        return `Sorry, you need at least $${item.price} to make that purchase`
+    } else if (item.quantity < 1) {
+        return `Sorry, there are none left`
+    } else {
+        return `Here are your ${item.name}`
+    }
+}
+
+function collectChange(change) {
+    total = 0
+    for (var i = 0; i < change.length; i++) {
+        total += change[i]
+    }
+    return total
+}
+
+module.exports = {
+    createItemStock,
+    makePurchase,
+    collectChange
+}
+
