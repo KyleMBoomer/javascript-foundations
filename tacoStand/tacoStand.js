7e3ac580a477c031247a67c017cacaeb47644fb4
“Kyle <kylemboomer@gmail.com>
2024-04-01 10:28:50 -0500

    Pass All Tests

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,33 @@
+function createIngredient(ingredient = 'unknown', price= 0.00) {
+    return {
+        name:ingredient,
+        price: price
+    }
+}
 
-module.exports = {}
+function createTaco(name = 'custom', ingredients = []) {
+    return {
+        name: name,
+        ingredients: ingredients
+    }
+}
+
+function addIngredientToTaco(taco, ingredient) {
+taco.ingredients.push(ingredient)
+return taco
+}
+
+function calculatePrice(taco) {
+    var price = 0
+    for (var i = 0; i < taco.ingredients.length; i++) {
+        price += taco.ingredients[i].price 
+    }
+    return price
+}
+
+module.exports = {
+    createIngredient,
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice
+}
